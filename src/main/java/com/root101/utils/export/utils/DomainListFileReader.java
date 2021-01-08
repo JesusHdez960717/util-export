cef57b87db18d56b2ade38305dc60cb1d12020d5
Unknown <unknown@example.com>
2021-01-08 15:47:08 -0500

    Licence a clases

--- src/main/java/com/root101/utils/export/utils/DomainListFileReader.java
+++ src/main/java/com/root101/utils/export/utils/DomainListFileReader.java
@@ -1,7 +1,18 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Copyright 2021 Root101 (jhernandezb96@gmail.com, +53-5-426-8660).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Or read it directly from LICENCE.txt file at the root of this project.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package com.root101.utils.export.utils;
 
@@ -11,8 +22,8 @@ import java.util.List;
 
 /**
  *
- * @author Jesus Hernandez Barrios (jhernandezb96@gmail.com)
- * @param <Domain>
+ * @author Root101 (jhernandezb96@gmail.com, +53-5-426-8660)
+ * @author JesusHdezWaterloo@Github
  */
 public interface DomainListFileReader<Domain extends DomainObject> {
 
